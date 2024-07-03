9e8a70cb3756bda8dc54b7e84e9153739ece3685
Ari Ghiffary <ari@MacBook-Air-Ari.local>
2024-07-03 15:47:58 +0700

    Update Config

---
+++ lib/config/app_config.dart
@@ -0,0 +1,7 @@
+import 'package:test_flavor/config/flavor_type_config.dart';
+
+class AppConfig {
+  final String appName;
+  final FlavorType flavor;
+  AppConfig({required this.appName, required this.flavor});
+}
---
+++ lib/main_dev.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:test_flavor/app.dart';
+import 'package:test_flavor/config/app_config.dart';
+import 'package:test_flavor/config/app_singleton_config.dart';
+import 'package:test_flavor/config/flavor_type_config.dart';
+
+Future<void> main() async {
+  // LOAD ENV
+  await dotenv.load(fileName: ".dev.env");
+
+  // Initialize your AppConfig
+  final prodAppConfig = AppConfig(
+    appName: 'Test Flutter Flavor',
+    flavor: FlavorType.dev,
+  );
+
+  // Set the global AppConfig
+  appConfigSingleton.config = prodAppConfig;
+
+  // Initialize and run the app
+  runApp(initializeApp(prodAppConfig));
+}
